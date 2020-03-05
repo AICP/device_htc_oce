17f66c5c7f1b558b92ba426df98974a6bee746c9
Siluxsept <ronny.horn.rh@gmail.com>
2020-03-05 02:57:35 +0100

    oce: rootdir: adjust / cleanup init
    Change-Id: I2e7d6d0fcaafc0acc1bfc3b162d3ef6fd9293b61

--- device.mk
+++ device.mk
@@ -281,7 +281,6 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
     init.qcom.sh \
     init.htc.fp.sh \
-    init.htc_oce.fp.rc \
     init.qcom.early_boot.sh \
     init.qcom.firmware_links.sh \
     init.target.rc \
