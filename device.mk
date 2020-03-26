3d3bbd289e219497839a04b979291bc613357796
Siluxsept <ronny.horn.rh@gmail.com>
2020-03-26 10:16:50 +0100

    Revert "Revert "oce: rootdir: add init.foreground.sh""
    This reverts commit 3cc1c1fdbe698c88c3c61dcc4da3fff202d30c74.
    
    Change-Id: I0c5bf215d2a874865c78a9d11df6109fd499eeeb

--- device.mk
+++ device.mk
@@ -293,6 +293,7 @@ PRODUCT_PACKAGES += \
     init.htc.fp.sh \
     init.qcom.early_boot.sh \
     init.recovery.qcom.rc \
+    init.foreground.sh \
     init.target.rc \
     init.oce.rc \
     init.qcom.power.rc \
